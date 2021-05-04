1203035d804514a3fdacb22268c5ed3f3f7def77
galihpermana29 <galihcbn123@gmail.com>
2021-05-04 19:42:03 +0700

    home, about

---
+++ main.js
@@ -0,0 +1,6 @@
+const toggleNav = document.querySelector('.hamburger');
+const links = document.querySelector('.links')
+
+toggleNav.addEventListener('click', function(){
+   links.classList.toggle('active')
+})
