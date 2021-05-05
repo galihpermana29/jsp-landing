463621d14dcb7de6eb7efb5bae299d09a355a0b1
galihpermana29 <galihcbn123@gmail.com>
2021-05-05 14:49:20 +0700

    link to every footer and content

--- main.js
+++ main.js
@@ -1,8 +1,21 @@
-
-
 const toggleNav = document.querySelector('.hamburger');
-const links = document.querySelector('.links')
+const links = document.querySelector('.links');
 
-toggleNav.addEventListener('click', function(){
-   links.classList.toggle('active')
-})
+toggleNav.addEventListener('click', function () {
+	links.classList.toggle('active');
+});
+
+const img = document.querySelector('.img img');
+console.log(img);
+
+let i = 1;
+setInterval(() => {
+   if(i == 4) i = 1;
+   img.setAttribute('src', `/assets/slide${i}.jfif`);
+   img.classList.add('slider')
+   setTimeout(()=>{
+      img.classList.remove('slider')
+   },1000)
+   i++;
+}, 2500);
+
