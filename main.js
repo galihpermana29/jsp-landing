990bd9163e1e0fa0c7a32539869bcbf47ea621f4
galihpermana29 <galihcbn123@gmail.com>
2021-05-10 22:27:57 +0700

    ad on index

--- main.js
+++ main.js
@@ -4,18 +4,37 @@ const links = document.querySelector('.links');
 toggleNav.addEventListener('click', function () {
 	links.classList.toggle('active');
 });
+if (window.location.pathname === '/pages/about.html') {
+	const img = document.querySelector('.img img');
+	// console.log(img);
+	if (img != null) {
+		let i = 1;
+		setInterval(() => {
+			if (i == 4) i = 1;
+			img.setAttribute('src', `/assets/slide${i}.jfif`);
+			img.classList.add('slider');
+			setTimeout(() => {
+				img.classList.remove('slider');
+			}, 1000);
+			i++;
+		}, 2500);
+	}
+}
 
-const img = document.querySelector('.img img');
-console.log(img);
+if (window.location.pathname === '/index.html') {
+	const overlay = document.querySelector('#overlay');
+	const closeAd = document.querySelector('.buttonOverlayClose');
+	const modal = document.querySelector('.modal');
 
-let i = 1;
-setInterval(() => {
-   if(i == 4) i = 1;
-   img.setAttribute('src', `/assets/slide${i}.jfif`);
-   img.classList.add('slider')
-   setTimeout(()=>{
-      img.classList.remove('slider')
-   },1000)
-   i++;
-}, 2500);
+	overlay.addEventListener('click', () => {
+		overlay.classList.remove('active');
+		modal.classList.remove('active');
+	});
 
+	closeAd.addEventListener('click', () => {
+		overlay.classList.remove('active');
+		modal.classList.remove('active');
+	});
+}
+
+// console.log(window.location.pathname);
